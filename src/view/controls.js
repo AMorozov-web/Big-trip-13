e7597229f9f15b272ea4d5ee4d24838bfc9daee2
AlexM <a.morozov.work@mail.ru>
2020-11-19 10:23:07 +0300

    CLose task

---
+++ src/view/controls.js
@@ -0,0 +1,14 @@
+import {createMenuTemplate} from './menu';
+import {createFiltersTemplate} from './filters';
+
+const createControlsTemplate = () => {
+  return `<h2 class="visually-hidden">Switch trip view</h2>
+
+          ${createMenuTemplate()}
+
+          <h2 class="visually-hidden">Filter events</h2>
+          
+          ${createFiltersTemplate()}`;
+};
+
+export {createControlsTemplate};
