af5f46cf62a74ac4375eaf8c85ab77dd4dfa6627
AlexM <a.morozov.work@mail.ru>
2020-12-01 23:27:43 +0300

    Add FilterTypes to consts

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -3,6 +3,13 @@ const RenderPositions = {
   BEFORE_END: `beforeend`,
 };
 
+const FilterTypes = {
+  EVERYTHING: `everything`,
+  FUTURE: `future`,
+  PAST: `past`,
+};
+
 export {
   RenderPositions,
+  FilterTypes,
 };
