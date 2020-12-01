ecf28f67bd01c95063c044510a1428d9e1415998
AlexM <a.morozov.work@mail.ru>
2020-12-01 23:32:25 +0300

    form-main fix

--- src/view/form/form-main.js
+++ src/view/form/form-main.js
@@ -73,7 +73,7 @@ const createEditEventFormTemplate = (event) => {
             <label class="visually-hidden" for="event-start-time-1">From</label>
             <input class="event__input  event__input--time" id="event-start-time-1"
               type="text" name="event-start-time" value="${dayjs(startTime).format(`DD/MM/YY HH:mm`)}">
-            —
+            &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
             <input class="event__input  event__input--time" id="event-end-time-1"
               type="text" name="event-end-time" value="${dayjs(endTime).format(`DD/MM/YY HH:mm`)}">
@@ -82,7 +82,7 @@ const createEditEventFormTemplate = (event) => {
           <div class="event__field-group  event__field-group--price">
             <label class="event__label" for="event-price-1">
               <span class="visually-hidden">Price</span>
-              €
+              &euro;
             </label>
             <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
           </div>
