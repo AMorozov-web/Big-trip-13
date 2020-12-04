cdf929b600c9d00833cc523b9f8fa047bf033d42
AlexM <a.morozov.work@mail.ru>
2020-12-04 18:41:09 +0300

    Refactored sort.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import {renderTemplate, renderElement} from './utils/utils';
 import TripInfoView from './view/site-info/site-info';
 import SiteControlsView from './view/site-controls/site-controls';
 import NewEventButtonView from './view/new-event-button';
-import {createSortTemplate} from './view/sort';
+import EventSortView from './view/sort';
 import {createListTemplate} from './view/list';
 import {events} from './view/list';
 
@@ -15,5 +15,5 @@ const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
 renderElement(tripMainElement, new TripInfoView(events).getElement(), RenderPosition.AFTER_BEGIN);
 renderElement(tripMainElement, new SiteControlsView().getElement(), RenderPosition.BEFORE_END);
 renderElement(tripMainElement, new NewEventButtonView().getElement(), RenderPosition.BEFORE_END);
-renderTemplate(tripEventsElement, createSortTemplate(), RenderPosition.BEFORE_END);
+renderElement(tripEventsElement, new EventSortView().getElement(), RenderPosition.BEFORE_END);
 renderTemplate(tripEventsElement, createListTemplate(), RenderPosition.BEFORE_END);
