6017287f803a547cf91c5d6a824600a6dbc7f75a
AlexM <a.morozov.work@mail.ru>
2020-12-07 07:23:40 +0300

    Sorting dev

--- src/utils.js
+++ src/utils.js
@@ -54,6 +54,19 @@ const calcDuration = (start, end) => {
   }
 };
 
+const getSortedFilms = (films, sortType) => {
+  switch (sortType) {
+    case SortType.DATE:
+      return films.slice().sort((a, b) => b.releaseDate - a.releaseDate);
+    case SortType.RATING:
+      return films.slice().sort((a, b) => b.rating - a.rating);
+    case SortType.COMMENTS:
+      return films.slice().sort((a, b) => b.comments.length - a.comments.length);
+    default:
+      return films;
+  }
+};
+
 const renderTemplate = (container, template, place) => container.insertAdjacentHTML(place, template);
 
 const renderElement = (container, element, place) => {
