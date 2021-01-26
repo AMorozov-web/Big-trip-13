29a8efb1bce4ef9cd1cd9dfd5a8e4db732f75ccb
AlexM <a.morozov.work@mail.ru>
2021-01-26 06:48:15 +0300

    Testing

--- src/presenter/point.js
+++ src/presenter/point.js
@@ -107,7 +107,7 @@ export default class Point {
     if (this._mode === Mode.EDIT) {
       this._changeData(
           UserAction.DELETE_POINT,
-          UpdateType.MAJOR,
+          UpdateType.MINOR,
           point
       );
       return;
